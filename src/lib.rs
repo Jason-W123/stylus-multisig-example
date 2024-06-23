2533166bdfd76c83a19cae18f847bc664b92911b
Jason-Wanxt <wanxutao123@gmail.com>
2024-06-23 19:07:15 +0800

    add constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,183 +1,3 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.24;
-
-// contract MultiSigWallet {
-//     event Deposit(address indexed sender, uint256 amount, uint256 balance);
-//     event SubmitTransaction(
-//         address indexed owner,
-//         uint256 indexed txIndex,
-//         address indexed to,
-//         uint256 value,
-//         bytes data
-//     );
-//     event ConfirmTransaction(address indexed owner, uint256 indexed txIndex);
-//     event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
-//     event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
-
-//     address[] public owners;
-//     mapping(address => bool) public isOwner;
-//     uint256 public numConfirmationsRequired;
-
-//     struct Transaction {
-//         address to;
-//         uint256 value;
-//         bytes data;
-//         bool executed;
-//         uint256 numConfirmations;
-//     }
-
-//     // mapping from tx index => owner => bool
-//     mapping(uint256 => mapping(address => bool)) public isConfirmed;
-
-//     Transaction[] public transactions;
-
-//     modifier onlyOwner() {
-//         require(isOwner[msg.sender], "not owner");
-//         _;
-//     }
-
-//     modifier txExists(uint256 _txIndex) {
-//         require(_txIndex < transactions.length, "tx does not exist");
-//         _;
-//     }
-
-//     modifier notExecuted(uint256 _txIndex) {
-//         require(!transactions[_txIndex].executed, "tx already executed");
-//         _;
-//     }
-
-//     modifier notConfirmed(uint256 _txIndex) {
-//         require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
-//         _;
-//     }
-
-//     constructor(address[] memory _owners, uint256 _numConfirmationsRequired) {
-//         require(_owners.length > 0, "owners required");
-//         require(
-//             _numConfirmationsRequired > 0
-//                 && _numConfirmationsRequired <= _owners.length,
-//             "invalid number of required confirmations"
-//         );
-
-//         for (uint256 i = 0; i < _owners.length; i++) {
-//             address owner = _owners[i];
-
-//             require(owner != address(0), "invalid owner");
-//             require(!isOwner[owner], "owner not unique");
-
-//             isOwner[owner] = true;
-//             owners.push(owner);
-//         }
-
-//         numConfirmationsRequired = _numConfirmationsRequired;
-//     }
-
-//     receive() external payable {
-//         emit Deposit(msg.sender, msg.value, address(this).balance);
-//     }
-
-//     function submitTransaction(address _to, uint256 _value, bytes memory _data)
-//         public
-//         onlyOwner
-//     {
-//         uint256 txIndex = transactions.length;
-
-//         transactions.push(
-//             Transaction({
-//                 to: _to,
-//                 value: _value,
-//                 data: _data,
-//                 executed: false,
-//                 numConfirmations: 0
-//             })
-//         );
-
-//         emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
-//     }
-
-//     function confirmTransaction(uint256 _txIndex)
-//         public
-//         onlyOwner
-//         txExists(_txIndex)
-//         notExecuted(_txIndex)
-//         notConfirmed(_txIndex)
-//     {
-//         Transaction storage transaction = transactions[_txIndex];
-//         transaction.numConfirmations += 1;
-//         isConfirmed[_txIndex][msg.sender] = true;
-
-//         emit ConfirmTransaction(msg.sender, _txIndex);
-//     }
-
-//     function executeTransaction(uint256 _txIndex)
-//         public
-//         onlyOwner
-//         txExists(_txIndex)
-//         notExecuted(_txIndex)
-//     {
-//         Transaction storage transaction = transactions[_txIndex];
-
-//         require(
-//             transaction.numConfirmations >= numConfirmationsRequired,
-//             "cannot execute tx"
-//         );
-
-//         transaction.executed = true;
-
-//         (bool success,) =
-//             transaction.to.call{value: transaction.value}(transaction.data);
-//         require(success, "tx failed");
-
-//         emit ExecuteTransaction(msg.sender, _txIndex);
-//     }
-
-//     function revokeConfirmation(uint256 _txIndex)
-//         public
-//         onlyOwner
-//         txExists(_txIndex)
-//         notExecuted(_txIndex)
-//     {
-//         Transaction storage transaction = transactions[_txIndex];
-
-//         require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
-
-//         transaction.numConfirmations -= 1;
-//         isConfirmed[_txIndex][msg.sender] = false;
-
-//         emit RevokeConfirmation(msg.sender, _txIndex);
-//     }
-
-//     function getOwners() public view returns (address[] memory) {
-//         return owners;
-//     }
-
-//     function getTransactionCount() public view returns (uint256) {
-//         return transactions.length;
-//     }
-
-//     function getTransaction(uint256 _txIndex)
-//         public
-//         view
-//         returns (
-//             address to,
-//             uint256 value,
-//             bytes memory data,
-//             bool executed,
-//             uint256 numConfirmations
-//         )
-//     {
-//         Transaction storage transaction = transactions[_txIndex];
-
-//         return (
-//             transaction.to,
-//             transaction.value,
-//             transaction.data,
-//             transaction.executed,
-//             transaction.numConfirmations
-//         );
-//     }
-// }
-
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
@@ -203,12 +23,17 @@ sol! {
     event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
     event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
 
+    error AlreadyInitialized();
     error ZeroOwners();
+    error InvaildConfirmationNumber();
+    error InvalidOwner();
+    error OwnerNotUnique();
     error NotOwner();
     error TxDoesNotExist();
     error TxAlreadyExecuted();
     error TxAlreadyConfirmed();
     error TxNotConfirmed();
+    error ConfirmationNumberNotEnough();
     error ExecuteFailed();
 }
 
@@ -238,18 +63,28 @@ sol_storage! {
 // Error types for the TimeLock contract
 #[derive(SolidityError)]
 pub enum MultiSigError {
+    AlreadyInitialized(AlreadyInitialized),
     ZeroOwners(ZeroOwners),
+    InvaildConfirmationNumber(InvaildConfirmationNumber),
+    InvalidOwner(InvalidOwner),
+    OwnerNotUnique(OwnerNotUnique),
     NotOwner(NotOwner),
     TxDoesNotExist(TxDoesNotExist),
     TxAlreadyExecuted(TxAlreadyExecuted),
     TxAlreadyConfirmed(TxAlreadyConfirmed),
     TxNotConfirmed(TxNotConfirmed),
+    ConfirmationNumberNotEnough(ConfirmationNumberNotEnough),
     ExecuteFailed(ExecuteFailed),
 }
 
 /// Declare that `Counter` is a contract with the following external methods.
 #[external]
 impl Counter {
+    pub fn num_confirmations_required(&self) -> Result<U256, MultiSigError> {
+        Ok(self.num_confirmations_required.get())
+    }
+
+    #[payable]
     pub fn deposit(&mut self) {
         let sender = msg::sender();
         let amount = msg::value();
@@ -281,6 +116,37 @@ impl Counter {
     }
 
 
+
+    pub fn initialize(&mut self, owners: Vec<Address>, num_confirmations_required: U256) -> Result<(), MultiSigError> {
+        if self.owners.len() > 0 {
+            return Err(MultiSigError::AlreadyInitialized(AlreadyInitialized{}));
+        }
+
+        if owners.len() == 0 {
+            return Err(MultiSigError::ZeroOwners(ZeroOwners{}));
+        }
+
+        if num_confirmations_required == U256::from(0) || num_confirmations_required > U256::from(owners.len()) {
+            return Err(MultiSigError::InvaildConfirmationNumber(InvaildConfirmationNumber{}));
+        }
+
+        for owner in owners.iter() {
+            if *owner == Address::default() {
+                return Err(MultiSigError::InvalidOwner(InvalidOwner{}))
+            }
+
+            if self.is_owner.get(*owner) {
+                return Err(MultiSigError::OwnerNotUnique(OwnerNotUnique{}))
+            }
+
+            self.is_owner.setter(*owner).set(true);
+            self.owners.push(*owner);
+        }
+
+        self.num_confirmations_required.set(num_confirmations_required);
+        Ok(())
+    }
+
     pub fn execute_transaction(&mut self, tx_index: U256) -> Result<(), MultiSigError>{
         let tx_index = tx_index.to::<usize>();
         if tx_index >= self.transactions.len() {
@@ -293,7 +159,7 @@ impl Counter {
             }
 
             if entry.num_confirmations.get() < self.num_confirmations_required.get() {
-                return Err(MultiSigError::TxAlreadyConfirmed(TxAlreadyConfirmed{}));
+                return Err(MultiSigError::ConfirmationNumberNotEnough(ConfirmationNumberNotEnough{}));
             }
             
             entry.executed.set(true);
